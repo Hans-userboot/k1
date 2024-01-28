72588bd7cf238be331b42efb6c93068ed719436a
Hans-userboot <100776056+Hans-userboot@users.noreply.github.com>
2024-01-28 09:04:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://zpmyoypk:c2ndyWQX7xQv4pd87PpVG6Rf_lxtR7Eo@satao.db.elephantsql.com/zpmyoypk")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002131472781"))
